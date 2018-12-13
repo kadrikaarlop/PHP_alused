bc112e0c5345068e383bf61354db2b6bb8eecedb
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-13 15:04:33 +0200

    Väljastus Print ja Echo

---
+++ Grin/index.php
@@ -0,0 +1,11 @@
+<?php
+
+//teksti väljastus print käsuga
+print 'See on esimene lause PHP-keeles'.'<br>';
+print 'See on teine lause'.'<br>';
+// teksti väljastus echo-käsuga
+echo 'See tekst on echo abil väljastatud'.'<br>';
+
+
+
+?>
