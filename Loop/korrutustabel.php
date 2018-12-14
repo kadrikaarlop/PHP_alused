0cc635b920a685fc45971f60a099fed1e2c508a9
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-14 10:50:30 +0200

    Korrutustabel

--- Loop/korrutustabel.php
+++ Loop/korrutustabel.php
@@ -1,2 +1,18 @@
 <?php
 
+echo '<h4>Tsüklid - for </h4>';
+
+echo '<table>';
+
+for($arv = 1; $arv <= 10; $arv1++){
+
+    echo '<tr>';
+    for($arv2 = 1; $arv2 <= 10; $arv2++) {
+        echo $arv1;
+    }
+    echo ($arv1 * $arv2).'<td>';
+}
+echo '<td style = "width: 20 px; text-align: center; border: solid black;">
+    echo '</td>';
+} echo '</tr>
+echo '</table>';
