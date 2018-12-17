4f3552cbf92ac40bca89608d3e4dcc163c24c6c5
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-17 14:02:40 +0200

    Funktsioonid

--- Functions/index.php
+++ Functions/index.php
@@ -1,2 +1,12 @@
-<?php
 
+<!doctype html>
+<html>
+<head>
+    <title>Funktsioonid -väljakutsumine -print()funktsioon</title>
+</head>
+<body>
+<?php
+$num = - 321;
+$newnum = abs ($num);
+print $newnum;
+?
