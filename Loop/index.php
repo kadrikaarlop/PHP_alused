c6a16ae2d1d4bc442aa80cb93c1a629381f034fa
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-17 13:23:13 +0200

    Lisati While tsükkel

--- Loop/index.php
+++ Loop/index.php
@@ -34,3 +34,23 @@ for($arv1 = 1; $arv1 <= 10; $arv1++){
     echo '</tr>';
 }
 echo '</table>';
+
+//funktsioon
+//function funktsiooniNimi($param1, $param2,...) {
+//tegevused, mis toimuvad funktsiooni sees
+//leia, mitu korda esineb siin number 5
+function numbriteKordumine($arv, $kontrollNumber) {
+    echo 'Arv on '. $arv . '<br>';
+    $numberKokku = 0;
+    while($arv != 0) {
+        $number = $arv % 10;
+        if($number == $kontrollNumber) {
+            $numberKokku = $numberKokku + 1;
+        }
+        $arv = $arv / 10;
+        settype($arv, 'int');
+    }
+    echo 'Number '.$kontrollNumber.' esineb antud arvus '. $numberKokku. ' korda<br>';
+}
+numbriteKordumine(442158755745, 5);
+$arv = 442158755745;
