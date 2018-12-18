6bce459b001eba018e80d74a9602d2b791dad159
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-18 12:32:10 +0200

    mees sinine, naine punane

--- Arrays/index.php
+++ Arrays/index.php
@@ -1,47 +1,25 @@
 <?php
 
-$kasutajad = array();
-    //tühi massiiv
-//andmete lisamine
+$kasutajad = array(
+    array(
+        'alice',
+        'Alice',
+        'Liddell',
+        'female'
+    ),
 
-$kasutajad[] = 'Alice';
-$kasutajad[] = 'Bob';
-array_push($kasutajad, 'Lucy');
+    array(
+        'bob',
+        'Bob',
+        'builder',
+        'male'
+    )
+);
 
-$lisakasutajad = array('mark', 'george');
-
-echo '<pre>';
-print_r($kasutajad);
-
-echo '</pre>';
-
-//var_dump($kasutajad);
-
-for($i = 0; $i < 3; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-
-//massiivi pikkus
-
-$kasutajadKokku =count($kasutajad);
-for($i = 0; $i < 3; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-
-array_shift($kasutajad);
-for($i = 0; $i < $kasutajadKokku; $i++){
-    echo $kasutajad[$i].'<br>';
-}
-
-$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
-    for($i = 0; $i < count($uuedKasutajad), $i);{
-    echo $uuedKasutajad[$i].'<br>';<
-}
-
-echo '<hr>';
-
-foreach ($uuedKasutajad as $kasutaja) {
-    echo $kasutaja.'<br>';
+foreach ($kasutajad as $kasutaja){
+    foreach ($kasutaja as $info){
+        echo $info.'<br>';
     }
-
+    echo '<hr>';
+}
 
