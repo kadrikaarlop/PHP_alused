db2429584d69e4c71ca3b687c9c2e91e4921c006
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-18 12:23:06 +0200

    arrayd

--- Arrays/index.php
+++ Arrays/index.php
@@ -2,6 +2,8 @@
 
 $kasutajad = array();
     //tühi massiiv
+//andmete lisamine
+
 $kasutajad[] = 'Alice';
 $kasutajad[] = 'Bob';
 array_push($kasutajad, 'Lucy');
@@ -14,5 +16,32 @@ print_r($kasutajad);
 echo '</pre>';
 
 //var_dump($kasutajad);
+
+for($i = 0; $i < 3; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+//massiivi pikkus
+
+$kasutajadKokku =count($kasutajad);
+for($i = 0; $i < 3; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+array_shift($kasutajad);
+for($i = 0; $i < $kasutajadKokku; $i++){
+    echo $kasutajad[$i].'<br>';
+}
+
+$uuedKasutajad = array_merge($kasutajad, $lisaKasutajad);
+    for($i = 0; $i < count($uuedKasutajad), $i);{
+    echo $uuedKasutajad[$i].'<br>';<
+}
+
+echo '<hr>';
+
+foreach ($uuedKasutajad as $kasutaja) {
+    echo $kasutaja.'<br>';
+    }
 
 
