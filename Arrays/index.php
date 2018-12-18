7d878627f7e39a722107ab0cbd15091baa1e945d
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-18 12:10:05 +0200

    Sisu lisamine

---
+++ Arrays/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$kasutajad = array();
+    //tühi massiiv
+$kasutajad[] = 'Alice';
+$kasutajad[] = 'Bob';
+array_push($kasutajad, 'Lucy');
+
+$lisakasutajad = array('mark', 'george');
+
+echo '<pre>';
+print_r($kasutajad);
+
+echo '</pre>';
+
+//var_dump($kasutajad);
+
+
