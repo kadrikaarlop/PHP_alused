bb57e40a198672e4f54d4ce4e92898ae996353c8
kadri.kaarlop <kadri.kaarlop@khk.ee>
2018-12-18 12:41:28 +0200

    mees sinine, naine punane

--- Arrays/index.php
+++ Arrays/index.php
@@ -15,11 +15,17 @@ $kasutajad = array(
         'male'
     )
 );
-
-foreach ($kasutajad as $kasutaja){
-    foreach ($kasutaja as $info){
-        echo $info.'<br>';
+for($i = 0; $i < count($kasutajad); $i++){
+    foreach ($kasutajad[$i] as $info){
+        if ($kasutajad[$i][3] == 'female'){
+        echo '<div style = "color: red">';
+        }   else {
+        echo '<div style = "color: blue">';
+        }
+    echo $info.'<div>';
     }
     echo '<hr>';
 }
+
+
 
