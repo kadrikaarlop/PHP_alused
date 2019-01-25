ba8c7c81567d4dbe2da1470b0ee8f3b522e91da7
kadrikaarlop <kadri.kaarlop@khkl.ee>
2019-01-25 16:11:35 +0200

    Lisati kera ruumala arvutamine

---
+++ Vorm/arvutamine.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kadriko
+ * Date: 25.01.2019
+ * Time: 15:58
+ */
+
+// ruumalade arvutamine
+
+//Kera Ruumala valem
+
+function keraRuumala($keraRaadius){
+    return 4/3 * pi() * pow($keraRaadius, 3);
+}
+// pow on astendamine pow(value, power), alguses väärtus ja siis aste
+// pi() on float - Pii=3,14
+
+
+?>
