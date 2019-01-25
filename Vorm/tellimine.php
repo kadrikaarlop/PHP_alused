624a0b410ad6d3a19cc6d286e83876f0e8abf071
kadrikaarlop <kadri.kaarlop@khkl.ee>
2019-01-25 15:35:37 +0200

    Vormist saadud andmed lisatakse muutujasse

--- Vorm/tellimine.php
+++ Vorm/tellimine.php
@@ -1,7 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kadri.kaarlop
- * Date: 22.01.2019
- * Time: 13:56
- */
+
+//lisab vormist saadud andmed muutujasse
+
+$toode1 = $_GET['t1'];
+$toode2 = $_GET['t2'];
+$toode3 = $_GET['t3'];
+echo 'Toode 1: '.$toode1.'tk<br>';
+echo 'Toode 2: '.$toode2.'tk<br>';
+echo 'Toode 3: '.$toode3.'tk<br>';
+?>
