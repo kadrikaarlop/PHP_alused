dc98659ebe92a56713cf3820086626f223dc7521
kadrikaarlop <kadri.kaarlop@khkl.ee>
2019-01-28 19:36:56 +0200

    Lisati tehted ajafunktsioonidega

--- Date/index.php
+++ Date/index.php
@@ -35,3 +35,34 @@ mktime(tunnid, minutid, sekundid, kuu, päev, aasta, suveaeg);
 echo '<br>';
 $sp = mktime(0,0,0,5,5,1983);
 echo date('d.m.Y', $sp);
+
+//Tehted kuupäevadega
+//sekundutega arvutamine
+echo date('d.m.Y G:i' , time()+60);			//1min pärast
+echo date('d.m.Y G:i' , time()+60*60);		//1h pärast
+echo date('d.m.Y G:i' , time()+60*60*24);	//24h pärast
+echo '<br>';
+
+//päevade, kuude, aastatega arvutamine
+$sp = mktime(0,0,0,10,29,1969-27);
+echo date('d.m.Y', $sp);			//29.10.1942
+echo '<br>';
+
+//arvutamine inglise keelsete lausetega
+echo strtotime("now");
+echo strtotime("tomorrow");
+echo strtotime("yesterday");
+echo strtotime("10 September 2000");
+echo strtotime("+1 day");
+echo strtotime("+1 week");
+echo strtotime("+2 week 3 days 4 hours 5 seconds");
+echo strtotime("next Thursday");
+echo strtotime("last Monday");
+echo strtotime("5pm + 6 Hours");
+echo strtotime("now + 4 fortnights");
+echo strtotime("last Monday");
+echo strtotime("4pm yesterday");
+echo strtotime("6am 10 days ago");
+echo'<br>';
+
+
